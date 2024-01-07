96ff3a6fac3b1d099e56ac53c8b093e0758609df
Firetest <codetestting@gmail.com>
2024-01-07 12:22:39 -0600

    TokTik -provider

---
+++ toktik/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/screens/discover/discover_screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => DiscoverProvider())],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme().getTheme(),
+          title: 'TokTik',
+          home: const DiscoverScreen()),
+    );
+  }
+}
